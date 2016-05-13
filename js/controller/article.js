5c0f501ed01daad1628d7c5f9b9433d1dfe14d89
itjiangzhuang <itjiangzhuang@163.com>
2016-05-13 19:19:47 +0800

    金额选择

--- js/controller/article.js
+++ js/controller/article.js
@@ -98,26 +98,62 @@ articleCtrl.controller('ArticleCreateStep1Ctrl', function ($http, $scope, $rootS
             console.log("login error");
         })
     };
-    
+    $scope.changeLoanValue = function(){
+    	$scope.createArticle.loanValue = $("#loanValue").val();
+    	$scope.$apply();
+    };
+
     $scope.changeLoanLife = function(){
     	$scope.createArticle.loanLife = $("#loanLife").val();
     	$scope.$apply();
-    }
+    };
     
     $scope.changeRateCap = function(){
     	$scope.createArticle.rateCap = $("#rateCap").val();
     	$scope.$apply();
-    }
+    };
     
     $scope.changeRateFloor = function(){
     	$scope.createArticle.rateFloor = $("#rateFloor").val();
     	$scope.$apply();
-    }
+    };
     
     $scope.init = function(){
     	if(!$scope.createArticle.id||isNullOrEmpty($scope.createArticle.id)){
 	    		$scope.getFormToken();
 	    }
+    	
+    	    $('#aomuntmoney').click( function(){
+		        $('#treelist_dummy').focus()
+		    });
+		    var i = Math.floor($('#treelist>li').length/2),
+		        j = Math.floor($('#treelist>li').eq(i).find('ul li').length /2);
+		    $("#treelist").mobiscroll().treelist({
+		        theme:"android-ics light",
+		        lang:"zh",
+		        defaultValue:[i,j],
+		        cancelText:null,
+		//            placeholder:"借款金额",
+		        headerText:function(valueText){return "借款金额";},
+		        formatResult:function(array){
+		            var text = $('#treelist>li').eq(array[0]).find('ul li').eq(array[1]).text().trim(' ');
+		            if(text=="万"){
+		                var w = 1;
+		            }else if(text == "十万"){
+		                var w = 10;
+		            }else if(text =="百万"){
+		                var  w = 100;
+		            }else if(text == "千万"){
+		                var w = 1000;
+		            }else if(text =="亿"){
+		                var w =10000
+		            }
+		            $('#loanValue').val($('#treelist>li').eq(array[0]).children('span').text()*w);
+		            $('#loanValue').change();
+		        }
+		
+		    });
+    	
     	$('#rate').daterate();//利率选择
     	$('#term').dateterm();//期限选择
     };
--- js/moneyamount.js
+++ js/moneyamount.js
@@ -2,37 +2,7 @@
  * Created by Administrator on 2016/5/13.
  */
 $(function(){
-    $('#aomuntmoney').click( function(){
-        $('#treelist_dummy').focus()
-    });
-    var i = Math.floor($('#treelist>li').length/2),
-        j = Math.floor($('#treelist>li').eq(i).find('ul li').length /2);
-    $("#treelist").mobiscroll().treelist({
-        theme:"android-ics light",
-        lang:"zh",
-        defaultValue:[i,j],
-        cancelText:null,
-//            placeholder:"借款金额",
-        headerText:function(valueText){return "借款金额";},
-        formatResult:function(array){
-            var text = $('#treelist>li').eq(array[0]).find('ul li').eq(array[1]).text().trim(' ');
-            if(text=="万"){
-                var w = 1;
-            }else if(text == "十万"){
-                var w = 10;
-            }else if(text =="百万"){
-                var  w = 100;
-            }else if(text == "千万"){
-                var w = 1000;
-            }else if(text =="亿"){
-                var w =10000
-            }
-            $('#amount').val($('#treelist>li').eq(array[0]).children('span').text()*w);
-            console.log($('#amount').val())
-         
-        }
 
-    })
     /*console.log($('#treelist_dummy').val())*/
 
 })
