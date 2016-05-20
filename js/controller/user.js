59768cab7b60f5e76ae5bde4bc6e14aad7649118
baiyang <459676797@qq.com>
2016-05-20 13:33:50 +0800

    聊天样式

--- js/controller/user.js
+++ js/controller/user.js
@@ -6,9 +6,28 @@ var userCtrl = angular.module('userCtrl', []);
 /*个人中心*/
 userCtrl.controller('UserCenterCtrl', function ($http, $scope, $rootScope, $location) {
 
-    $scope.userQuestion = function () {
-        $location.path('/user/question');
+    $scope.userQuestion = function () {//打开问题中心
+        $location.path('/user/questions');
     };
+    $scope.messageCenter = function () {//打开消息中心
+        $location.path('/user/messages');
+    };
+    $scope.myWallet = function () {//打开我的钱包
+        $location.path('/user/wallet');
+    };
+    $scope.windControl = function () {//打开？？
+        $location.path('/user/wind_control');
+    };
+    $scope.setting = function () {
+        $location.path('/user/setting');
+    };
+    $scope.investProjects = function () {
+        $location.path('/invest_projects');
+    };
+    $scope.goto_create = function () {
+
+        $location.path("/article/create/step1");
+    }
 
     $scope.init = function () {
         $http({
@@ -33,16 +52,17 @@ userCtrl.controller('UserCenterCtrl', function ($http, $scope, $rootScope, $loca
         });
     };
 
-    $scope.show = function(articleId,toUser){
-    	$location.path("/article/questions/" + articleId+ "/" + toUser);
-    /*切换普通用户和vip*/
-    $scope.names = 2;
-$scope.vipToCommon = function(){
-    if ($scope.names == 1) {
-        $('.vipHave').css('display', 'none');
-        $('.vipNone').css('display', 'block');
+    $scope.show = function(articleId,toUser) {
+        $location.path("/article/questions/" + articleId + "/" + toUser);
+        /*切换普通用户和vip*/
+        $scope.names = 2;
+        $scope.vipToCommon = function () {
+            if ($scope.names == 1) {
+                $('.vipHave').css('display', 'none');
+                $('.vipNone').css('display', 'block');
+            }
+        }
     }
-}
 });
 
 userCtrl.controller('MyQuestionsCtrl', function ($http, $scope, $rootScope, $location) {
